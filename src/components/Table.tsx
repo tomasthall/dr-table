71fe84066a8cd854706f016efb21b082c2182b37
tomasthall <gebol1@onet.eu>
2020-05-12 22:09:57 +0200

    refactor(app): refactor styles, add fixed last column, change configs

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -24,31 +24,53 @@ const animationName = keyframes`
 const TableHeader = styled.th<{width: number}>`
     cursor: pointer;
     width: ${props => props.width.toString()}px;
+    height: 100px;
     border-right: 1px solid white;
     border-bottom: 1px solid white;
     &:last-of-type {
         color: red;
+        position: absolute;
+        right: 0;
+        top: 1px;
+        z-index: 3;
+        border-left: 1px solid white;
     }
+    background: #3d1e7a;
+    font-weight: 700;
+    font-size: 1.3rem;
 `;
 
-const StyledTable = styled.table`
+const StyledTable = styled.table<{lastColumnWidth: number}>`
     overflow-x: scroll;
     color: white;
     border: 1px solid white;
     border-collapse: separate;
     border-spacing: 0;
     table-layout: fixed;
+    width: 1px;
+    height: 100%;
+    padding-right: ${props => props.lastColumnWidth + 2}px;
 `;
 
 const TableRow = styled.tr`
+    position: relative;
+    width: 100%;
+    height: auto;
 `;
 
-const TableCell = styled.td<{lastCell?: boolean}>`
+const TableCell = styled.td<{lastCell?: boolean, lastColumnWidth?: number}>`
     border: 1px solid white;
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
     animation: ${animationName} 0.3s;
+    background: #151a67;
+    ${props => props.lastCell && props.lastColumnWidth &&`
+        right: 0;
+        position: absolute;
+        top: auto;
+        width: ${props.lastColumnWidth}px;
+    `}
 `;
 
 function hasKey<O>(obj: O, key: keyof any): key is keyof O {
@@ -71,8 +93,9 @@ class Table extends Component<Props, State> {
     render() {
         const {config, data} = this.props;
         const {currentSort} = this.state;
+        const lastColumnWidth: number = config.tableHeaders[config.tableHeaders.length - 1].columnWidth;
         return (
-            <StyledTable>
+            <StyledTable lastColumnWidth={lastColumnWidth}>
                 <TableRow>{config.tableHeaders.map(header => <TableHeader width={header.columnWidth} onClick={() => this.setSorting(header.columnName)}>{header.columnName}</TableHeader>)}</TableRow>
                 {(data.sort((left, right): number => { if(hasKey(left, currentSort)) { if(left[currentSort] > right[currentSort]) return 1; if(left[currentSort] < right[currentSort]) return -1; } return 0; } )).map(tableRow => <TableRow>
                     <TableCell>{tableRow.id}</TableCell>
@@ -81,7 +104,7 @@ class Table extends Component<Props, State> {
                     <TableCell>{tableRow.profession}</TableCell>
                     <TableCell>{tableRow.salary}</TableCell>
                     <TableCell>{tableRow.location}</TableCell>
-                    <TableCell lastCell>{tableRow.available ? '' : 'not'} available</TableCell>
+                    <TableCell lastCell lastColumnWidth={lastColumnWidth}>{tableRow.available ? '' : 'not'} available</TableCell>
                 </TableRow>)}
             </StyledTable>
         )
